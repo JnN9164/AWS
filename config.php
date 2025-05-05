fddc0af58d4a116f392293a3bceed9ec22f44e19
JnN9164 <140533610+JnN9164@users.noreply.github.com>
2025-05-05 21:21:55 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = ''; 
+$host = 'graduation.cp8dkm9ksdvu.us-east-1.rds.amazonaws.com';
+$user = 'admin';
+$pass = 'nbuser123';
 $db = 'graduation_store';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
@@ -10,9 +10,8 @@ if (!$conn) {
     die('Database connection failed: ' . mysqli_connect_error());
 }
 
-
 $admin_username = 'admin';
-$admin_password_plain = 'admin123';  
+$admin_password_plain = 'admin123';
 $admin_role = 'admin';
 
 
@@ -20,9 +19,9 @@ $check_admin_query = "SELECT * FROM users WHERE username = '$admin_username' LIM
 $check_result = mysqli_query($conn, $check_admin_query);
 
 if (mysqli_num_rows($check_result) === 0) {
-   
+
     $hashed_password = password_hash($admin_password_plain, PASSWORD_DEFAULT);
-    $insert_admin_query = "INSERT INTO users (username, password, role) 
+    $insert_admin_query = "INSERT INTO users (username, password, role)
                            VALUES ('$admin_username', '$hashed_password', '$admin_role')";
     mysqli_query($conn, $insert_admin_query);
 }
