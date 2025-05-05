7a03c713d24b9d44f2e0ae77debce7f284f8eabd
JnN9164 <140533610+JnN9164@users.noreply.github.com>
2025-05-05 20:59:05 +0800

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -6,19 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $product_id = $_POST['product_id'];
     $quantity = $_POST['quantity'] ?? 1;
 
-    
+
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = [];
     }
 
-    
+
     if (isset($_SESSION['cart'][$product_id])) {
         $_SESSION['cart'][$product_id] += $quantity;
     } else {
         $_SESSION['cart'][$product_id] = $quantity;
     }
 
-    
+
     $cart_count = array_sum($_SESSION['cart']);
 
     echo json_encode(['success' => true, 'cart_count' => $cart_count]);
